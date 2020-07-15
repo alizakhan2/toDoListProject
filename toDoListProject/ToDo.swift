346c7d69b3466c1eb5d58e6896024fb2700c2215
aliza khan <alizakhan@students.ccusd.org>
2020-07-15 13:34:05 -0700

    test commit

---
+++ toDoListProject/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  toDoListProject
+//
+//  Created by Aliza Khan on 7/14/20.
+//  Copyright © 2020 Aliza Khan. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false 
+    
+}
